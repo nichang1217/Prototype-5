12b7007fa42da000b6ebc2e5bf5ad62c8fea08a5
nichang1217 <122054069+nichang1217@users.noreply.github.com>
2023-04-04 10:04:58 -0700

    5.1 done

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -9,15 +9,17 @@ public class Target : MonoBehaviour
     private float maxSpeed = 16;
     private float maxTorque = 10;
     private float xRange = 4;
-    private float ySpawnPos = -6;
+    private float ySpawnPos = -2;
 
     // Start is called before the first frame update
     void Start()
     {
         targetRb = GetComponent<Rigidbody>();
 
-        targetRb.AddForce(RandomForce, ForceMode.Impulse);
-        targetRb.AddTorque(RandomTorque, RandomTorque, RandomTorque, ForceMode.Impulse);
+        targetRb.AddForce(RandomForce(), ForceMode.Impulse);
+        targetRb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
+
+        transform.position = RandomSpawnPos();
 
     }
 
@@ -25,6 +27,16 @@ public class Target : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void OnMouseDown()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
     }
 
     Vector3 RandomForce()
